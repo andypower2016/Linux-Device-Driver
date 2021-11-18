844fe5a64e6fd6a1fd8aa265a5f2bd0aca40a325
andypower2016 <73726447+andypower2016@users.noreply.github.com>
2021-11-18 23:30:45 +0800

    kmalloc/kfree

--- char_device.c
+++ char_device.c
@@ -5,22 +5,27 @@
 #include <linux/semaphore.h>   // used to access semaphores for syncronization
 #include <asm/uaccess.h>	// copy_to_user, copy_from_user (used to map data from user space to kernel space)
 
+#include <linux/slab.h> // kmalloc/kfree
+
+#define DEVICE_NAME "char_device"
 
 // fake char device structure
 struct char_device 
 {
-    char buffer[256];
-    struct semaphore sem;
-} device ;
+    char buffer[256];	   // device data
+    struct semaphore sem; // semaphore, for locking the device
+    struct cdev *cdev;    // cdev
+};
+
+static struct char_device *fake_device = NULL;
 
 // To register a fake char device, we need cdev and other variables
-struct cdev *mcdev; // m stands 'my'
-int major_num; // will store our major number, extracted from dev_t using macro - mknod /dev/device_file c major minor
-int ret;       // holds the return value of each kernel function
-dev_t dev_num; // device number
+static int major_num; // will store our major number, extracted from dev_t using macro - mknod /dev/device_file c major minor
+static int ret;       // holds the return value of each kernel function
+static dev_t dev_num; // device number
 
-#define DEVICE_NAME "char_device"
 
+// file operations
 int open(struct inode *, struct file *);
 ssize_t read(struct file *, char __user *, size_t, loff_t *);
 ssize_t write(struct file *, const char __user *, size_t, loff_t *);
@@ -33,7 +38,50 @@ struct file_operations fileop =
     .read = read,
     .write = write,
     .release = release,
+    //.ioctl = ioctl,
+    //.llseek = llseek,
 };
+
+static void cleanup_device(void)
+{
+	if(fake_device)
+	{
+	   cdev_del(fake_device->cdev);
+	   kfree(fake_device);
+	}
+	unregister_chrdev_region(dev_num, 1);
+}
+
+static int setup_device(void)
+{
+        ret = 0; // 0 for success
+	fake_device = kmalloc(sizeof(struct char_device), GFP_KERNEL);
+	if(!fake_device)
+	{
+	   printk(KERN_ALERT "Fail to kalloc device\n");
+	   ret = -ENOMEM;
+	   cleanup_device();
+	   return ret;
+	}
+	memset(fake_device, 0, sizeof(struct char_device));	
+	
+	// init data
+	sprintf(fake_device->buffer, "Init data for device");
+	
+	// init semaphore
+	sema_init(&fake_device->sem, 1); // initial value one, means nothing is locked
+	
+	// init cdev
+	fake_device->cdev = cdev_alloc(); 
+	fake_device->cdev->ops = &fileop;
+	fake_device->cdev->owner = THIS_MODULE;
+	ret = cdev_add(fake_device->cdev, dev_num, 1);
+	if(ret < 0)
+	{
+	   printk(KERN_ALERT "Fail to add cdev to kernel\n");
+	}
+	return ret;
+}
 
 static __init int chrdev_init(void)
 {
@@ -48,6 +96,7 @@ static __init int chrdev_init(void)
 	printk(KERN_INFO "Major number=%d\n", major_num);
 	printk(KERN_INFO "Use mknod /dev/%s c %d 0\" for device file\n", DEVICE_NAME, major_num);
 	
+	/*
 	// (2) create cdev structure, initialize cdev
 	mcdev = cdev_alloc(); 
 	mcdev->ops = &fileop;
@@ -63,13 +112,22 @@ static __init int chrdev_init(void)
 	
 	//(4) Initialize semaphore
 	sema_init(&device.sem, 1); // initial value one, means nothing is locked
-	return 0;
+	
+	// init device data
+	sprintf(device.buffer, "Init data for device");
+	
+	*/
+	
+	
+	// setup device dynamically
+	ret = setup_device();
+	
+	return ret;
 }
 
 static __exit void chrdev_exit(void)
 {
-	cdev_del(mcdev);
-	unregister_chrdev_region(dev_num, 1);
+	cleanup_device();
 	printk(KERN_ALERT "Unload Device Complete\n");
 }
 
@@ -78,10 +136,10 @@ int open(struct inode *pinode, struct file *pfile)
 {
 	printk(KERN_ALERT "Inside %s Function\n", __FUNCTION__);
 	// Only allow one process to open this device by using a semaphore as a mutex
-	if(down_interruptible(&device.sem) != 0) // sem = sem - 1
+	if(down_interruptible(&fake_device->sem) != 0) // sem = sem - 1
 	{
 	   printk(KERN_ALERT "can't lock device when open\n");
-	   return ret;	
+	   return -1;	
 	}
 	printk(KERN_INFO "opened device ! \n");
 	return 0;
@@ -91,10 +149,10 @@ int open(struct inode *pinode, struct file *pfile)
 ssize_t read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset)
 {
 	printk(KERN_ALERT "Inside %s Function\n", __FUNCTION__);
-	printk(KERN_INFO "Reading from device, data=%s\n", device.buffer);
+	printk(KERN_INFO "Reading from device, data=%s\n", fake_device->buffer);
 	// Take data from kernel space to user space
 	// copy_from_user(destination, source, sizeToTransfer)
-	ret = copy_to_user(buffer, device.buffer, length);
+	ret = copy_to_user(buffer, fake_device->buffer, length);
 	return ret;
 }
 
@@ -105,7 +163,7 @@ ssize_t write(struct file *pfile, const char __user *buffer, size_t length, loff
 	printk(KERN_INFO "Writing to device , data=%s\n", buffer);
 	// Take data from user space to kernel space
 	// copy_from_user(destination, source, sizeToTransfer)
-	ret = copy_from_user(device.buffer, buffer, length);
+	ret = copy_from_user(fake_device->buffer, buffer, length);
 	return ret;
 }
 
@@ -113,12 +171,25 @@ ssize_t write(struct file *pfile, const char __user *buffer, size_t length, loff
 int release(struct inode *pinode, struct file *pfile)
 {
 	printk(KERN_ALERT "Inside %s Function\n", __FUNCTION__);
-	up(&device.sem);  // sem = sem + 1
+	up(&fake_device->sem);  // sem = sem + 1
 	printk(KERN_INFO "Closed device\n");
 	return 0;
 }
 
+/*
+loff_t llseek(struct file* pfile, loff_t *f_pos, int n)
+{
+
+	return 0;
+}
+
+int ioctl(struct inode* pinode, struct file* pfile, unsigned int f_flags, unsigned long )
+{
+	
+}*/
+
 module_init(chrdev_init);
 module_exit(chrdev_exit);
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("Dual BSD/GPL");
+//MODULE_LICENSE("GPL");
 
